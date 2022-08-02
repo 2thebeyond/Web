443a3560da72929a2e20899567b3ec481f031583
장준영 <jynmeme@gmail.com>
2022-08-02 07:59:08 +0000

    Chore: Support dark mode on PC

--- colors.js
+++ colors.js
@@ -1,64 +1,112 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-   	var background = document.querySelector("body");
-	var ol = document.querySelector('ol');
-	var a = document.querySelector('a');
-	var div = document.querySelector("div");
-	var nav = document.querySelector(".nav");
-	var btnName = document.querySelector(".header");
-	var currentTheme = localStorage.getItem("currentTheme");
-	const btnElement = document.getElementById("darkmode");
-	const dark = '<div>Dark Mode</div>';
-	const light = '<div>Light Mode</div>';
-	var btnName = document.querySelector('#darkmode');
+var background = document.querySelector("body");
+var ol = document.querySelector('ol');
+var a = document.querySelector('a');
+var div = document.querySelector("div");
+var nav = document.querySelector(".nav");
+var btnName = document.querySelector(".header");
+var currentTheme = localStorage.getItem("currentTheme");
+const btnElement = document.getElementById("darkmode");
+const dark = '<div>Dark Mode</div>';
+const light = '<div>Light Mode</div>';
+var btnName = document.querySelector('#darkmode');
 
-	loadTheme();
+loadTheme();
 
-	function loadTheme(){
-		if (currentTheme == "dark") {
-			background.setAttribute("style", "background-color: black; color: white;");
-			ol.setAttribute("style", "color: white;");
-			a.setAttribute("style", "color: white;");
-			div.setAttribute("style", "color: white;");
-			btnName.setAttribute("value", "Light Mode");
-			// console.log("현재 테마 : 다크");
-		} else{
-			background.setAttribute("style", "background-color: white; color: black;");
-			ol.setAttribute("style", "color: black;");
-			a.setAttribute("style", "color: black;");
-			div.setAttribute("style", "color: black;");
-			btnName.setAttribute("value", "Dark Mode");
-			// console.log("현재 테마 : 라이트");
-		}
+function loadTheme(){
+	if (currentTheme == "dark") {
+		background.setAttribute("style", "background-color: black; color: white;");
+		ol.setAttribute("style", "color: white;");
+		a.setAttribute("style", "color: white;");
+		div.setAttribute("style", "color: white;");
+		btnName.setAttribute("value", "Light Mode");
+		// console.log("현재 테마 : 다크");
+	} else{
+		background.setAttribute("style", "background-color: white; color: black;");
+		ol.setAttribute("style", "color: black;");
+		a.setAttribute("style", "color: black;");
+		div.setAttribute("style", "color: black;");
+		btnName.setAttribute("value", "Dark Mode");
+		// console.log("현재 테마 : 라이트");
 	}
-
-	function ToggleTheme(self){
-		const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-		if (prefersDarkScheme.matches) {
-			window.matchMedia("(prefers-color-scheme: light)").matches ? 'light' : 'dark';
-		} else {
-			window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-		}
-
-		const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-		localStorage.setItem('currentTheme', newTheme);
-
-		location.reload();
-	}
-} else {
-	var background = document.querySelector("body");
-	var ol = document.querySelector('ol');
-	var a = document.querySelector('a');
-	var div = document.querySelector("div");
-	var nav = document.querySelector(".nav");
-	var btnName = document.querySelector(".header");
-	var currentTheme = localStorage.getItem("currentTheme");
-	background.setAttribute("style", "background-color: white; color: black;");
-	ol.setAttribute("style", "color: black;");
-	a.setAttribute("style", "color: black;");
-	div.setAttribute("style", "color: black;");
-	btnName.setAttribute("value", "Dark Mode");
 }
+
+function ToggleTheme(self){
+	const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+	if (prefersDarkScheme.matches) {
+		window.matchMedia("(prefers-color-scheme: light)").matches ? 'light' : 'dark';
+	} else {
+		window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+	}
+
+	const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+	localStorage.setItem('currentTheme', newTheme);
+
+	location.reload();
+} 
+
+
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//    	var background = document.querySelector("body");
+// 	// var ol = document.querySelector('ol');
+// 	var a = document.querySelector('a');
+// 	var div = document.querySelector("div");
+// 	var nav = document.querySelector(".nav");
+// 	var btnName = document.querySelector(".header");
+// 	var currentTheme = localStorage.getItem("currentTheme");
+// 	const btnElement = document.getElementById("darkmode");
+// 	const dark = '<div>Dark Mode</div>';
+// 	const light = '<div>Light Mode</div>';
+// 	var btnName = document.querySelector('#darkmode');
+
+// 	loadTheme();
+
+// 	function loadTheme(){
+// 		if (currentTheme == "dark") {
+// 			background.setAttribute("style", "background-color: black; color: white;");
+// 			// ol.setAttribute("style", "color: white;");
+// 			a.setAttribute("style", "color: white;");
+// 			div.setAttribute("style", "color: white;");
+// 			btnName.setAttribute("value", "Light Mode");
+// 			// console.log("현재 테마 : 다크");
+// 		} else{
+// 			background.setAttribute("style", "background-color: white; color: black;");
+// 			// ol.setAttribute("style", "color: black;");
+// 			a.setAttribute("style", "color: black;");
+// 			div.setAttribute("style", "color: black;");
+// 			btnName.setAttribute("value", "Dark Mode");
+// 			// console.log("현재 테마 : 라이트");
+// 		}
+// 	}
+
+// 	function ToggleTheme(self){
+// 		const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// 		if (prefersDarkScheme.matches) {
+// 			window.matchMedia("(prefers-color-scheme: light)").matches ? 'light' : 'dark';
+// 		} else {
+// 			window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+// 		}
+
+// 		const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+// 		localStorage.setItem('currentTheme', newTheme);
+
+// 		location.reload();
+// 	}
+// } else {
+// 	var background = document.querySelector("body");
+// 	// var ol = document.querySelector('ol');
+// 	var a = document.querySelector('a');
+// 	var div = document.querySelector("div");
+// 	var nav = document.querySelector(".nav");
+// 	var btnName = document.querySelector(".header");
+// 	var currentTheme = localStorage.getItem("currentTheme");
+// 	background.setAttribute("style", "background-color: white; color: black;");
+// 	// ol.setAttribute("style", "color: black;");
+// 	a.setAttribute("style", "color: black;");
+// 	div.setAttribute("style", "color: black;");
+// 	btnName.setAttribute("value", "Dark Mode");
+// }
 
 // var Body = {
 // 	setFontColor:function(color){
