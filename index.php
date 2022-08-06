6388a193ceb6a0448b220d868523f956b7626619
장준영 <jynmeme@gmail.com>
2022-08-06 10:05:43 +0000

    Refactor: Refactor index page

--- index.php
+++ index.php
@@ -1,67 +1,73 @@
 <?php 
 session_start();
 require('lib/print.php');
-$conn = mysqli_connect('localhost','root','test','level1');
-$sql = "SELECT * FROM forum";
-// $sql_ = "SELECT * FROM forum";
+$conn = mysqli_connect(
+	'localhost',
+	'root',
+	'test',
+	'level1'
+);
 mysqli_set_charset($conn, "utf8");
+$sql = "SELECT * FROM forum";
 $result = mysqli_query($conn, $sql);
-// $result_ = mysqli_query($conn, $sql);
-// $query = "SELECT * FROM forum LEFT JOIN member ON forum.author_id = member.id LIMIT 0, 10";
 
+///////////////////////// PAGINATION 
 $list_num = 5;
+$page_num = 5;
+
 if (isset($_GET['vpage'])){
 	$vpage = $_GET['vpage'];
 } else {
 	$vpage = 1;
 }
+
 $v_page = (int)$vpage;
 $index_no = ($v_page - 1) * $list_num;
 
+$postQuery = "SELECT * FROM forum ORDER BY id";
+$total_post = mysqli_query($conn, $postQuery);
+$total_post_result = mysqli_num_rows($total_post);
+
 $query = "SELECT * FROM forum ORDER BY id DESC LIMIT {$index_no}, {$list_num}";
-$query2 = "SELECT * FROM forum ORDER BY id DESC";
 $data = mysqli_query($conn, $query);
-$data2 = mysqli_query($conn, $query2);
-$total_posts = mysqli_num_rows($data2);
-// $total_posts = mysqli_num_rows($data);
-// $lastpage = (int)ceil((double)$total_posts / 3);
 
-$page_num = 5;
-
-$total_page = ceil($total_posts / $list_num);
+$total_page = ceil($total_post_result / $list_num);
 $now_block = ceil($vpage / $page_num);
+
 $s_pageNum = ($now_block - 1) * $page_num + 1;
 if ($s_pageNum <= 0){
 	$s_pageNum = 1;
 }
+
 $e_pageNum = (int)($now_block * $page_num);
 if($e_pageNum > $total_page){
 	$e_pageNum = $total_page;
 }
 
-
-// $list = '';
-$arr = array();
+///////////////////////// POST LIST
+$postIdArr = array();
+$nickArr = array();
+$titleArr = array();
 $descArr = array();
-$arr2 = array();
-$arr3 = array();
+
 $i = 0;
 while($row = mysqli_fetch_array($data)){
 	$sql = "SELECT * FROM forum LEFT JOIN member ON forum.author_id = member.id WHERE forum.id = {$row['id']}";
 	$result = mysqli_query($conn, $sql);
 	$row_ = mysqli_fetch_array($result);
-	$arr3[$i] = htmlspecialchars($row_['user_nick']);
+	
+	$nickArr[$i] = htmlspecialchars($row_['user_nick']);
 	$escaped_title = htmlspecialchars($row['title']);
 	$escaped_description = htmlspecialchars($row['description']);
-	$arr[$i] = $escaped_title;
+	
+	$titleArr[$i] = $escaped_title;
 	$descArr[$i] = $escaped_description;
-	$colored_title_ = "<div style=''>{$escaped_title}</div>";
-	// $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$colored_title}</a></li>";
-	$arr2[$i] = $row['id'];
+	$postIdArr[$i] = $row['id'];
+	
 	$i++;
 }
-// var_dump($arr);
 
+///////////////////////// POST CONTENTS
 $article = array(
 	'title'=>'Welcome :)',
 	'description'=>'홈페이지에 오신 것을 환영합니다.'
@@ -69,7 +75,6 @@ $article = array(
 $title = 'Welcome :)';
 if(isset($_GET['id'])){
 	$filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
-	// $sql = "SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id = {$filtered_id}";
 	$sql = "SELECT * FROM forum LEFT JOIN member ON forum.author_id = member.id WHERE forum.id = {$filtered_id}";
 	$result = mysqli_query($conn, $sql);
 	echo mysqli_error($conn);
@@ -79,24 +84,16 @@ if(isset($_GET['id'])){
 	$article['date'] = htmlspecialchars($row['created']);
 	$article['nick'] = htmlspecialchars($row['user_nick']);
 	$article['author_id'] = htmlspecialchars($row['author_id']);
-	// unset( $_SESSION['curPost_id']);
 	$_SESSION['curPost_id'] = $_GET['id'];
-	// echo $_SESSION['curPost_id'];
 	if (empty($article['nick'])){
 		$author = "<p>unknown</p>";
-		
-	} else {
+	} 
+	else {
 		$author = "<p>{$article['nick']}</p>";
 	}
 } else {
 	$_SESSION['curPost_id'] = '0';
-	// unset( $_SESSION['curPost_id']);
-	// echo $_SESSION['curPost_id'];
-	// unset($_SESSION['userName']);
 }
-//print_r($article);
-
-// $update = '<a href="update.php?id='.$_GET['id'].'">update</a>';
 ?>
 <!doctype html>
 <html>
@@ -116,34 +113,31 @@ if(isset($_GET['id'])){
 		<div class="nav">
 			<div class="nav_header">
 				<div class="nav_account_btns" style="width: 
-				<?php if(isset($_SESSION['id'])) { ?> 560px; <?php  } else { ?> 460px; <?php } ?> ">
-				<?php if(isset($_SESSION['id'])) { ?>
+					<?php if(isset($_SESSION['id'])) { ?> 560px; <?php  } else { ?> 460px; <?php } ?> ">
+					<?php if(isset($_SESSION['id'])) { ?>
 					<button class="nav_btn" type="button" onclick="location.href='mypage/mypage_view.php'">마이페이지</button>
 					<button class="nav_btn" type="button" onclick="location.href='mypage/logout.php'">로그아웃</button>
-				<?php  } else { ?>
+					<?php } else { ?>
 					<button class="nav_btn" type="button" onclick="location.href='register_view.php'">회원가입</button>
 					<button class="nav_btn" type="button" onclick="location.href='login_view.php'">로그인</button>
-				<?php } ?>
+					<?php } ?>
 				</div>
 				<div class="nav_post_btns" style="width: 
-				<?php if(isset($_SESSION['id'])) { ?>
-				<?php if($_SESSION['id'] == $article['author_id']) { ?> 520px; <?php } else { ?>
-				195px; <?php } } ?> "> 
-				<?php if(isset($_SESSION['id'])) {  ?>
+					<?php if(isset($_SESSION['id'])) { ?>
+					<?php if($_SESSION['id'] == $article['author_id']) { ?> 520px; <?php } else { ?> 195px; <?php } } ?> "> 
+					<?php if(isset($_SESSION['id'])) {  ?>
 					<button class="nav_btn" type="button" onclick="location.href='create.php'">글쓰기</button> 
-				<?php } ?>
-				<?php if(isset($_GET['id']) && isset($_SESSION['id'])) { ?>
-				<?php if($_SESSION['id'] == $article['author_id']) { ?>	
+					<?php } ?>
+					<?php if(isset($_GET['id']) && isset($_SESSION['id'])) { ?>
+					<?php if($_SESSION['id'] == $article['author_id']) { ?>	
 					<button class="nav_btn" type="button" onclick="location.href='update.php?id=<?=htmlspecialchars($_GET['id'])?>'">수정</button>
 					<button class="nav_btn" type="submit" form="delete" style="color:red;">삭제</button>
 						<form id="delete" action="delete_process.php" method="post">
-							<!-- <input type="hidden" name="id" value="htmlspecialchars($_GET['id'])?>"> -->
 							<input type="hidden" name="id" value="<?=$_GET['id']?>">
 						</form>
-				<?php }	} ?>
+					<?php }	} ?>
 				</div>
 			</div>
-			<!-- list -->
 		</div>
 		<div id="article">
 			<?php if(isset($_GET['id'])) { ?>
@@ -156,33 +150,25 @@ if(isset($_GET['id'])){
 
 							if(preg_match($mobile_agent, $_SERVER['HTTP_USER_AGENT'])){
 								?> <img src="images/profiles/default/blank_profile_picture.png" style="width:80px; height:80px; padding: 0px; margin: 0px;"/> <?php
-							}else{
+							} else {
 								?> <img src="images/profiles/default/blank_profile_picture.png" style="width:60px; height:60px; padding: 0px; margin: 0px;"/> <?php
 							}
-						} else {
-							//
+						} 
+						?>
+					</td>
+					<td style="font-weight:bold; padding-left:20px;">
+						<?php 
+						if ($_GET['id'] != '0'){
+							echo $author;
 						}
 						?>
 					</td>
-				<td style="font-weight:bold; padding-left:20px;">
-					<?php 
-					//print_title();
-					if ($_GET['id'] != '0'){
-						echo $author;
-					} else {
-						//
-					}
-					//echo $article['name'];
-					?>
-				</td>
 				</tr>
 				<tr>
 					<td style="padding-left:20px;"> 
 						<?php
 						if ($_GET['id'] != '0'){
 							echo $article['date'];
-						} else {
-							//
 						}
 						?>
 					</td>
@@ -191,7 +177,6 @@ if(isset($_GET['id'])){
 			<?php } ?>
 			<h2 style="overflow: auto; margin-top: 20px">
 				<?php
-				//print_description();
 				if ($_GET['id'] != '0'){
 					echo $article['title'];
 				} else {
@@ -210,47 +195,38 @@ if(isset($_GET['id'])){
 			</p>
 		</div>
 		<div class="list">
-			<!-- <table id="list" style="width: 100%; "> -->
 			<?php
 			$j = 0;
-			// $k = count($arr) -1;
-			while ($j < count($arr)){
-				
-				// 현재창 열기
+			while ($j < count($titleArr)){
 				echo "<table class='list_layout' border=1; style='border-left: 0px black solid; border-right: 0px black solid;> 
 						<tr style='border-width: 6px; border-style: solid;'>
 							<td class='profile_label' rowspan='3' style='width:50px; border:0px;'> 
 								<img class='profile_img' src='images/profiles/default/blank_profile_picture.png'/>
 							</td>
-							<td class='title_label' style='border:0px; font-weight: bold;' onclick=location.href='index.php?id={$arr2[$j]}'>
-								{$arr[$j]}
+							<td class='title_label' style='border:0px; font-weight: bold;' onclick=location.href='index.php?id={$postIdArr[$j]}'>
+								{$titleArr[$j]}
 							</td>
 						</tr>
 						<tr>
-							<td class='desc_label' style='border:0px; font-size: 80%;' onclick=location.href='index.php?id={$arr2[$j]}'>
+							<td class='desc_label' style='border:0px; font-size: 80%;' onclick=location.href='index.php?id={$postIdArr[$j]}'>
 							{$descArr[$j]}
 							</td>
 						</tr>
 						<tr>
-							<td class='nick_label' style='border:0px; font-size: 70%;' onclick=location.href='index.php?id={$arr2[$j]}'>
-								{$arr3[$j]}
+							<td class='nick_label' style='border:0px; font-size: 70%;' onclick=location.href='index.php?id={$postIdArr[$j]}'>
+								{$nickArr[$j]}
 							</td>
 						</tr>
 					</table>";
-				// 새로운창 열기
-				// echo "<li onclick=window.open(\"index.php?id={$arr2[$j]}\")>{$arr[$j]}</li>";
 				$j++;
-				// $k--;
 			}
-			//echo $list;
 			?>
-			
 		</div>
 		<div class="pagination">
 			<?php
 			if($vpage <= 1){
     		?>
-		 	<span onclick="location.href='index.php?id=<?php echo $_SESSION['curPost_id']?>&vpage=1'">이전</span>
+			<span onclick="location.href='index.php?id=<?php echo $_SESSION['curPost_id']?>&vpage=1'">이전</span>
 			<?php } else{ ?>
 			<span onclick="location.href='index.php?id=<?php echo $_SESSION['curPost_id'];?>&vpage=<?php echo ($vpage-1);?>'">이전</span>
 			<?php }; 
